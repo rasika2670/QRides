faf24cbf53d1d614cc3e42c7e856606b92d682e6
anni02th <aniketmore2000th@gmail.com>
2024-04-09 14:18:33 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./*/*.{html,js,ts,jsx,tsx}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily:{
