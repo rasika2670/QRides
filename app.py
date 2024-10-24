cc47ea5298503d50ffe6b013721d3be52cbc328e
darshanak20 <darshanakherde20@gmail.com>
2024-10-24 14:29:43 +0530

    commit

--- app.py
+++ app.py
@@ -1,80 +1,84 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Database initialization
-def init_db():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS vehicles (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            company_name TEXT,
-            bike_name TEXT,
-            mileage INTEGER,
-            owner_name TEXT,
-            owner_license INTEGER,
-            from_time TEXT,
-            to_time TEXT,
-            vehicle_no TEXT,
-            phone INTEGER,
-            email TEXT,
-            address TEXT,
-            image1 BLOB,
-            image2 BLOB
-        )
-    ''')
-    conn.commit()
-    conn.close()
 
-# Route for the lend form
-@app.route('/lend', methods=['GET', 'POST'])
-def lend():
-    if request.method == 'POST':
-        company_name = request.form['company_name']
-        bike_name = request.form['bike_name']
-        mileage = request.form['mileage']
-        owner_name = request.form['owner_name']
-        owner_license = request.form['owner_license']
-        from_time = request.form['from_time']
-        to_time = request.form['to_time']
-        vehicle_no = request.form['vehicle_no']
-        phone = request.form['phone']
-        email = request.form['email']
-        address = request.form['address']
-        image1 = request.files['image1'].read()
-        image2 = request.files['image2'].read()
+@app.route('/')
+def index():
+    return render_template('index.html')  
+"""
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-        conn = sqlite3.connect('database.db')
-        c = conn.cursor()
-        c.execute('''
-            INSERT INTO vehicles (company_name, bike_name, mileage, owner_name, owner_license, from_time, to_time, vehicle_no, phone, email, address, image1, image2)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (company_name, bike_name, mileage, owner_name, owner_license, from_time, to_time, vehicle_no, phone, email, address, image1, image2))
-        conn.commit()
-        conn.close()
-        
-        return redirect(url_for('rent'))
+@app.route("/navig")
+def navig():
+    return render_template('navig.html') 
 
-    return render_template('lend.html')
+@app.route("/help")
+def help():
+    return render_template('help.html')
 
-# Route for the rent page
+@app.route("/login")
+def login():
+    return render_template('login.html') 
+
 @app.route('/rent')
 def rent():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM vehicles')
-    rent_data = c.fetchall()
-    conn.close()
+    return render_template('rent.html')
 
-    return render_template('rent.html', rent_data=rent_data)
 
-# Route for the thank you page
-@app.route('/thankyou')
+@app.route("/booking")
+def booking():
+    return render_template('booking.html') 
+
+@app.route("/customerdetails")
+def customerdetails():
+    return render_template('customerdetails.html') 
+
+@app.route("/edit")
+def edit():
+    return render_template('edit.html')
+
+@app.route("/Lend")
+def Lend():
+    return render_template('Lend.html')
+
+@app.route("/owner")
+def owner():
+    return render_template('owner.html')
+
+@app.route("/page4")
+def page4():
+    return render_template('page4.html')
+
+@app.route("/payment")
+def payment():
+    return render_template('payment.html')
+
+@app.route("/product")
+def product():
+    return render_template('product.html')
+
+@app.route("/rent")
+def rent():
+    return render_template('rent.html')
+
+@app.route("/thankyou")
 def thankyou():
     return render_template('thankyou.html')
 
+@app.route("/tp")
+def tp():
+    return render_template('tp.html')
+
+@app.route("upload")
+def upload():
+    return render_template('upload.html')
+"""
+
+
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True)
+
